cd00e201cd4d4d45b5324dfc60e56a9f4a1071d5
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-01-10 20:14:03 -0700

    styling the daylistitem when hover + available

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,15 @@
 import React from "react";
+import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+   const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots===0
+   })
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
@@ -14,3 +20,7 @@ export default function DayListItem(props) {
 // The <li> represents the entire day item
 // The <h2> should display the day name
 // The <h3> should display the spots remaining for a day
+// when spots === 0 apply the .day-list__item--full styles
+// day-list__item all the time
+// day-list__item--selected class name if props.selected is true
+// day-list__item--full class name if props.spots is 0.
