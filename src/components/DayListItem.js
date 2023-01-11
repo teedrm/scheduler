3de47ac79ed1dd78dd43245642ced13768ff36d2
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-01-10 20:46:33 -0700

    making sure all tests past for daylistitem-spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,24 +3,30 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-   const dayClass = classNames('day-list__item', {
-    'day-list__item--selected': props.selected,
-    'day-list__item--full': props.spots===0
-   })
+  const formatSpots = function (spots) {
+    if (!spots) {
+      return "no spots remaining";
+    } else if (spots === 1) {
+      return `${spots} spot remaining`;
+    } else {
+      return `${spots} spots remaining`;
+    }
+  };
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
+  });
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li
+      className={dayClass}
+      onClick={() => {
+        props.setDay(props.name);
+      }}
+    >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
-
-
-// The <li> represents the entire day item
-// The <h2> should display the day name
-// The <h3> should display the spots remaining for a day
-// when spots === 0 apply the .day-list__item--full styles
-// day-list__item all the time
-// day-list__item--selected class name if props.selected is true
-// day-list__item--full class name if props.spots is 0.
