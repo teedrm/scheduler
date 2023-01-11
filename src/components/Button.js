581c15f5d171132feeed61eff2725d1a75388f6f
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-01-10 17:31:51 -0700

    styling buttons and rendering styles depending on the state

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
-
 import "components/Button.scss";
+import classNames from "classnames";
+
+//classNames('foo', { bar: true }); // => 'foo bar'
+// classNames('foo', { bar: false }); // => 'foo'
 
 export default function Button(props) {
-   return <></>;
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
