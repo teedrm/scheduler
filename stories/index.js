5120c9a18a1485612666a32a0c53e5af40252f24
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-01-11 20:35:17 -0700

    error pop up with close option

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -156,4 +157,10 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   ))
-  .add("Status", () => <Status message={"Deleting"} />)
+  .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => (
+    <Error 
+    message="Could not delete appointment."
+    onClose={action('onClose')}
+    />
+  ))
