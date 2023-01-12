c5c530047ea7704513e1e5ffd9ec3597f369acde
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-01-11 20:29:51 -0700

    deleting loading spinner

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
+import Status from "components/Appointment/Status.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -154,4 +155,5 @@ storiesOf("Appointment", module)
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
     />
-  ));
+  ))
+  .add("Status", () => <Status message={"Deleting"} />)
