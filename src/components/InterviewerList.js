28060875228f2f0dd07e6e530fd7af898d90f5cb
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-01-11 19:02:40 -0700

    update refactoring interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,17 +3,17 @@ import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function DayListItem(props) {
-    const interviewersArray = props.interviewers.map((interviewer) => {
-        return (
-            <InterviewerListItem 
-            key={interviewer.id}
-            name={interviewer.name}
-            avatar={interviewer.avatar}
-            selected={interviewer.id === props.interviewer} 
-            setInterviewer = {(event) => props.setInterviewer(interviewer.id)}
-            />
-        )
-    })
+  const interviewersArray = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
+      />
+    );
+  });
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
