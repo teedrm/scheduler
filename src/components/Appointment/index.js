b23829545305e02a772bef850f5e80d9f0b53175
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-01-16 22:07:59 -0700

    fixing updated interviewer id

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -73,7 +73,7 @@ export default function Appointment(props) {
       {mode === EDIT && (
         <Form
           student={props.interview.student}
-          interviewer={props.interview.interviewer}
+          interviewer={props.interview.interviewer.id}
           interviewers={props.interviewers}
           onCancel={back}
           onSave={save}
