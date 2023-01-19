ff51059d45bdb7ef96cacaa098312bc228321217
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-01-18 17:28:25 -0700

    added validation and error messages when student name is blank or interviewer is  not selected. Mock function to call count - making sure onSave button is working

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import Button from "components/Button";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("");
@@ -16,7 +17,17 @@ export default function Form(props) {
     props.onCancel();
   }
 
-  const save = () => {
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    setError("")
     props.onSave(student, interviewer);
   }
 
@@ -31,7 +42,9 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={props.interviewers}
@@ -42,7 +55,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={save}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { render } from "@testing-library/react";
+
+import Application from "components/Application";
+import Appointment from "components/Appointment";
+
+
+it("renders without crashing", () => {
+  render(<Application />);
+});
+
+describe("Appointment", () => {
+    it("renders without crashing", () => {
+        render(<Appointment />);
+    });
+});
+
+//it declares test
+//test can be used in place of it
+ 
